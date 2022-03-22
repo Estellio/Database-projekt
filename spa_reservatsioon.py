4a43cc60e9d4a57c3d8471e9b5ce64ee60f8779b
Estellio <94623286+Estellio@users.noreply.github.com>
2022-03-22 23:18:40 +0200

    Add files via upload

--- spa_reservatsioon.py
+++ spa_reservatsioon.py
@@ -1,7 +1,8 @@
 import sqlite3
 
-choice = input('Reservatsiooni olemasolu kontrollimiseks valige "K", uue lisamiseks "U" \n')
-ühendus = sqlite3.connect('spa_database')
+choice = input('Reservatsiooni olemasolu kontrollimiseks sisestagage "K", uue lisamiseks "U" \n')
+fail = open('spa_reservatsioonid.txt', 'r', encoding = 'utf-8')
+ühendus = sqlite3.connect('spa_database.db')
 con = ühendus.cursor()
 
 con.execute('''CREATE TABLE IF NOT EXISTS spa_reservatsioonid
@@ -11,18 +12,47 @@ Inimesed INTEGER,
 Kellaaeg INTEGER)
 ''')
 
-with open('spa_reservatsioonid.txt', 'r', encoding = 'utf-8') as fail:
-    for rida in fail:
-        elemendid = rida.strip('\n').split(';')
+for rida in fail:
+    elemendid = rida.strip('\n').split(';')
+    kontroll = con.execute(('SELECT * FROM spa_reservatsioonid WHERE Eesnimi = ? AND Perekonnanimi = ?'),(elemendid[0], elemendid[1]))
+    check = kontroll.fetchall()
+    if not check:
         con.execute('''INSERT INTO spa_reservatsioonid
 (Eesnimi, Perekonnanimi, Inimesed, Kellaaeg)
 VALUES
-(?, ?, ?, ?)''', (elemendid[0], elemendid[1], elemendid[2], elemendid[3]))
+(?, ?, ?, ?)''', (elemendid))
         ühendus.commit()
+    else:
+        continue
+fail.close()
+
+def uus_reserv():
+    eesnimi = input('Nimi: ')
+    perekonnanimi = input('Perekonnanimi: ')
+    inimesed = input('Inimeste arv: ')
+    kellaaeg = input('Kellaaeg: ')
+    con.execute('INSERT INTO spa_reservatsioonid(Eesnimi, Perekonnanimi, Inimesed, Kellaaeg) VALUES (?, ?, ?, ?)', (eesnimi, perekonnanimi, inimesed, kellaaeg))
+    ühendus.commit()
+    ühendus.close()
+
+def kontroll():
+    eesnimi = input('Nimi: ')
+    p_nimi = input('Perekonnanimi: ')
+    # perekonnanime ees on tühik
+    kontroll = con.execute(('SELECT * FROM spa_reservatsioonid WHERE Eesnimi = ? AND Perekonnanimi = ?'), (eesnimi, p_nimi))
+    check = kontroll.fetchall()
+    if not check:
+        ask = input('Reserveeringut ei ole andmebaasis. Kas soovite selle lisada? (Jah/Ei) ')
+        if ask.lower() == 'jah':
+            uus_reserv()
+            ühendus.commit()
+    else:
+        print('Reserveering on olemas.')
 
 if choice.lower() == 'k':
-    print('Kontroll')
+    kontroll()
 elif choice.lower() == 'u':
-    print('Uus reservatsioon')
+    uus_reserv()
+    print('Uus reservatsioon on lisatud')
 else:
     print('Päring ebaõnnestus')
